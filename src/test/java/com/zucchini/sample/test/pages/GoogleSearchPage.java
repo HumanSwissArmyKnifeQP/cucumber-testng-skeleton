cc3a09c72c0c1b6e3534fa6c3b052d5fe222baa4
HumanSwissArmyKnifeQP <quopperleaf@email.com>
2018-06-06 22:27:53 -0700

    fix examples

--- src/test/java/com/zucchini/sample/test/pages/GoogleSearchPage.java
+++ src/test/java/com/zucchini/sample/test/pages/GoogleSearchPage.java
@@ -1,7 +1,10 @@
 package com.zucchini.sample.test.pages;
 
 import com.zucchini.framework.ui.AbstractPage;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * @author humanswissarmyknife
@@ -10,11 +13,19 @@ public class GoogleSearchPage extends AbstractPage {
     
     public static final String URL = "https://www.google.ca";
     
+    @FindBy(css="input#lst-ib")
+    protected WebElement searchBox;
+    
     public GoogleSearchPage(WebDriver driver){
         super(driver);
     }
     
     public static GoogleSearchPage go(WebDriver driver){
         return goToPage(driver, GoogleSearchPage.class, URL);
+    }
+    
+    public GoogleSearchPage search(String searchString){
+        searchBox.sendKeys(searchString + Keys.ENTER);
+        return createPage(driver, GoogleSearchPage.class);
     }
 }
--- src/test/java/com/zucchini/sample/test/stepdefs/BrowserStepdefs.java
+++ src/test/java/com/zucchini/sample/test/stepdefs/BrowserStepdefs.java
@@ -1,6 +1,7 @@
 package com.zucchini.sample.test.stepdefs;
 
 import com.zucchini.framework.test.AbstractZucchiniGlue;
+import com.zucchini.sample.test.pages.GoogleSearchPage;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -8,18 +9,21 @@ import org.openqa.selenium.WebDriver;
  */
 public class BrowserStepdefs extends AbstractZucchiniGlue {
     
+    private GoogleSearchPage googlePage;
+    
+    private WebDriver driver;
+    
     public BrowserStepdefs() {
         
         super();
         
         Given("^I am on the Google Search page", ()->{
-            WebDriver driver = super.getDriver();
-            driver.get("https://www.google.ca");
-            Thread.sleep(1000);
+            driver = getDriver();
+            googlePage = GoogleSearchPage.go(driver);
         });
         
         And("^I search for (.*)$", (String s)->{
-            
+            googlePage.search(s);
         });
         
         Then("^we pass", ()->{
